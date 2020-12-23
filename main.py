36ddadcc238f7a8da3b7dddf9d3f49b87632611e
Sedatyf <gaxel@hotmail.fr>
2020-12-23 12:19:15 +0100

    Add argparse in main()

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dotenv
 import os
 from check_multiple_mail import check_multiple_mail
+import argparse
 
 #account credentials
 found = dotenv.find_dotenv('config.env')
@@ -8,4 +9,9 @@ dotenv.load_dotenv(found)
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
 
-check_multiple_mail(USERNAME, PASSWORD, 6)
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--multiple", type=int, help="Allow you to check multiple attachments")
+args = parser.parse_args()
+
+if args.multiple:
+	check_multiple_mail(USERNAME, PASSWORD, args.multiple)
