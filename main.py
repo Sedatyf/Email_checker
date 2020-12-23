b09c69024ba6dbd58a43ff2acd52267d3e42657f
Sedatyf <gaxel@hotmail.fr>
2020-12-23 12:51:07 +0100

    Add search_mail and argparse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import dotenv
-import os
+import dotenv, os, argparse
 from check_multiple_mail import check_multiple_mail
-import argparse
+from search_mail import search_mail
 
 #account credentials
 found = dotenv.find_dotenv('config.env')
@@ -10,8 +9,11 @@ USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--search", type=str, help="Search for a specific mail")
 parser.add_argument("-m", "--multiple", type=int, help="Allow you to check multiple attachments")
 args = parser.parse_args()
 
-if args.multiple:
+if args.search:
+	search_mail(USERNAME, PASSWORD, args.search)
+elif args.multiple:
 	check_multiple_mail(USERNAME, PASSWORD, args.multiple)
---
+++ search_mail.py
@@ -0,0 +1,8 @@
+import imaplib
+import email
+from email.header import decode_header
+from dotenv import load_dotenv
+import os
+
+def search_mail(username, password, objet):
+	print("todo")
